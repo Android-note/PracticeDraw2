57040f36e20f1c608d8313b143263c74806b41b4
yangyi <yangyirunning@163.com>
2017-08-13 17:04:57 +0800

    2017年08月13日17:04:56

--- app/src/main/java/com/hencoder/hencoderpracticedraw2/practice/Practice01LinearGradientView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw2/practice/Practice01LinearGradientView.java
@@ -13,6 +13,8 @@ import android.view.View;
 
 import com.hencoder.hencoderpracticedraw2.R;
 
+import java.util.Locale;
+
 public class Practice01LinearGradientView extends View {
 
     //首先声明和初始化的时候就已经在构造器中设置为抗锯齿
@@ -33,7 +35,6 @@ public class Practice01LinearGradientView extends View {
     {
         // 用 Paint.setShader(shader) 设置一个 LinearGradient
         // LinearGradient 的参数：坐标：(100, 100) 到 (500, 500) ；颜色：#E91E63 到 #2196F3
-
         paint.setShader(new LinearGradient(100, 100, 500, 500,
                 ContextCompat.getColor(getContext(), R.color.color1),
                 ContextCompat.getColor(getContext(), R.color.color2),
